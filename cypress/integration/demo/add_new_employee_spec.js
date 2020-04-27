a294600f030beea9510e48db4de6e21c03f1a6bc
Willy De la Cruz Sierra <will.delacruz.sierra@gmail.com>
2020-04-26 21:46:46 -0500

    Adding: Add employee spec - new spec: add_new_employee_spec.js - bumping-up NPM module versions

---
+++ cypress/integration/demo/add_new_employee_spec.js
@@ -0,0 +1,20 @@
+/// <reference types="cypress" />
+import { v4 as uuidv4 } from 'uuid';
+const faker = require('faker');
+
+context('Actions', () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:8080/')
+    })
+
+    it('Add a new employee', () => {
+        let employee_name = faker.name.findName();
+        let employee_email = faker.internet.email()
+        let success_message = "Employee successfully added"
+        cy.get('#employee-name-txt').type(employee_name);
+        cy.get('#employee-email-txt').type(employee_email)
+        cy.get('#add-employee-btn').click()
+        cy.contains(success_message)
+        //cy.get('tr').eq(-1).find('td').eq(0).should('contain', employee_name)
+    })
+})
--- src/components/EmployeeForm.vue
+++ src/components/EmployeeForm.vue
@@ -1,102 +1,105 @@
 <template>
-  <div id="employee-form">
-    <form @submit.prevent="handleSubmit">
+    <div id="employee-form">
+        <form @submit.prevent="handleSubmit">
 
-      <label>Employee name</label>
-      <input
-        ref="first"
-        type="text"
-        :class="{ 'has-error': submitting && invalidName }"
-        v-model="employee.name"
-        @focus="clearStatus"
-        @keypress="clearStatus"
-      >
-      <label>Employee Email</label>
-      <input
-        type="text"
-        :class="{ 'has-error': submitting && invalidEmail }"
-        v-model="employee.email"
-        @focus="clearStatus"
-      >
-      <p
-        v-if="error && submitting"
-        class="error-message"
-      >❗Please fill out all required fields</p>
-      <p
-        v-if="success"
-        class="success-message"
-      >✅ Employee successfully added</p>
-      <button>Add Employee</button>
-    </form>
+            <label>Employee name</label>
+            <input
+                    id="employee-name-txt"
+                    ref="first"
+                    type="text"
+                    :class="{ 'has-error': submitting && invalidName }"
+                    v-model="employee.name"
+                    @focus="clearStatus"
+                    @keypress="clearStatus"
+            >
+            <label>Employee Email</label>
+            <input
+                    id="employee-email-txt"
+                    type="text"
+                    :class="{ 'has-error': submitting && invalidEmail }"
+                    v-model="employee.email"
+                    @focus="clearStatus"
+            >
+            <p
+                    v-if="error && submitting"
+                    class="error-message"
+            >❗Please fill out all required fields</p>
+            <p
+                    v-if="success"
+                    class="success-message"
+            >✅ Employee successfully added</p>
+            <button id="add-employee-btn">Add Employee</button>
+        </form>
 
-  </div>
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'employee-form',
-  data() {
-    return {
-      error: false,
-      submitting: false,
-      success: false,
-      employee: {
-        name: '',
-        email: '',
-      }
+    export default {
+        name: 'employee-form',
+        data() {
+            return {
+                error: false,
+                submitting: false,
+                success: false,
+                employee: {
+                    name: '',
+                    email: '',
+                }
+            }
+        },
+        computed: {
+            invalidName() {
+                return this.employee.name === ''
+            },
+
+            invalidEmail() {
+                return this.employee.email === ''
+            },
+        },
+        methods: {
+            handleSubmit() {
+                this.clearStatus()
+                this.submitting = true
+
+                if (this.invalidName || this.invalidEmail) {
+                    this.error = true
+                    return
+                }
+
+                this.$emit('add:employee', this.employee)
+                this.$refs.first.focus()
+                this.employee = {
+                    name: '',
+                    email: '',
+                }
+                this.success = true
+                this.error = false
+                this.submitting = false
+            },
+
+            clearStatus() {
+                this.success = false
+                this.error = false
+            }
+        }
     }
-  },
-  computed: {
-    invalidName() {
-      return this.employee.name === ''
-    },
-
-    invalidEmail() {
-      return this.employee.email === ''
-    },
-  },
-  methods: {
-    handleSubmit() {
-      this.clearStatus()
-      this.submitting = true
-
-      if (this.invalidName || this.invalidEmail) {
-        this.error = true
-        return
-      }
-
-      this.$emit('add:employee', this.employee)
-      this.$refs.first.focus()
-      this.employee = {
-        name: '',
-        email: '',
-      }
-      this.success = true
-      this.error = false
-      this.submitting = false
-    },
-
-    clearStatus() {
-      this.success = false
-      this.error = false
-    }
-  }}
 </script>
 
 <style scoped>
-form {
-  margin-bottom: 2rem;
-}
+    form {
+        margin-bottom: 2rem;
+    }
 
-[class*="-message"] {
-  font-weight: 500;
-}
+    [class*="-message"] {
+        font-weight: 500;
+    }
 
-.error-message {
-  color: #d33c40;
-}
+    .error-message {
+        color: #d33c40;
+    }
 
-.success-message {
-  color: #32a95d;
-}
+    .success-message {
+        color: #32a95d;
+    }
 </style>
